a745a5d6a79431b8d689cf7364071186fbcd237e
danyelaristizabal <danyelaristizabal@hotmail.com>
2025-01-27 21:10:54 -0500

    - changes on telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,10 +8,6 @@ vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({
         search = vim.fn.input("Grep > "),
         results_title = "Search Results",
-        layout_strategy = "horizontal",
-        layout_config = {
-            height = 0.4, -- Adjust the height as needed
-        },
         attach_mappings = function(_, map)
             map('i', '<CR>', require('telescope.actions').send_to_qflist + require('telescope.actions').open_qflist)
             map('n', '<CR>', require('telescope.actions').send_to_qflist + require('telescope.actions').open_qflist)
